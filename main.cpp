4cd98214eeb9ecb7dc9f9a15eb715dc8a65b3cea
MC-bulka <56048386+MC-bulka@users.noreply.github.com>
2019-10-23 09:03:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  4laba4
+//
+//  Created by Katerina Miklashevskaya on 22.10.2019.
+//  Copyright © 2019 Katerina Miklashevskaya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
